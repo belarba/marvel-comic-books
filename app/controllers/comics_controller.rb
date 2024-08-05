d5d4c18cdafd466dfe90862ef3ac3df82367fa7e
Daniel Belarmino <belarba@gmail.com>
2024-08-05 18:27:09 +0100

    filtering by character

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,6 +1,12 @@
 class ComicsController < ApplicationController
   def index
-    @comics = MarvelService.new.comics
+    @comics = if params[:character]
+                character_id = MarvelService.new.character_id(params[:character])
+                character_id ? MarvelService.new.character_comics(character_id) : { 'data' => { 'results' => [] } }
+              else
+                MarvelService.new.comics
+              end
+
     Rails.logger.debug "Comics response: #{@comics.inspect}"
   end
 end
--- app/services/marvel_service.rb
+++ app/services/marvel_service.rb
@@ -7,21 +7,51 @@ class MarvelService
     @public_key = ENV['MARVEL_PUBLIC_KEY']
     @private_key = ENV['MARVEL_PRIVATE_KEY']
     @hash = generate_hash
+    @base_query = {
+      apikey: @public_key,
+      ts: @timestamp,
+      hash: @hash,
+      limit: 5
+    }
   end
 
   def comics(offset = 0)
-    options = {
-      query: {
-        apikey: @public_key,
-        ts: @timestamp,
-        hash: @hash,
-        offset: offset
-      }
-    }
+    cache_key = "marvel_comics_#{offset}"
+    Rails.cache.fetch(cache_key, expires_in: 1.day) do
+      options = { query: @base_query.merge(offset: offset) }
+      response = self.class.get('/comics', options)
+      Rails.logger.debug "Marvel API response: #{response.body}"
+      response.parsed_response
+    end
+  end
 
-    response = self.class.get('/comics', options)
-    Rails.logger.debug "Marvel API response: #{response.body}"
-    response.parsed_response
+  def character_id(name)
+    cache_key = "marvel_character_id_#{name}"
+    Rails.cache.fetch(cache_key, expires_in: 1.day) do
+      options = { query: @base_query.merge(name: name) }
+      response = self.class.get('/characters', options)
+      Rails.logger.debug "Marvel API response character id: #{response.body}"
+      parsed_response = response.parsed_response
+      if parsed_response['data'] && parsed_response['data']['results'] && !parsed_response['data']['results'].empty?
+        id = parsed_response['data']['results'].first['id']
+        Rails.logger.debug "Marvel API response character id: #{id}"
+        id
+      else
+        nil
+      end
+    end
+  end
+
+  def character_comics(id, offset = 0)
+    p "entrou no character comicsdddddd"
+    p id
+    cache_key = "marvel_character_comics_#{id}_#{offset}"
+    Rails.cache.fetch(cache_key, expires_in: 1.day) do
+      options = { query: @base_query.merge(offset: offset) }
+      response = self.class.get("/characters/#{id}/comics", options)
+      Rails.logger.debug "Character Comics response: #{response.body}"
+      response.parsed_response
+    end
   end
 
   private
